b35fbe3ed25d4e1d3e5f9c3221984da411768ba1
Ifavhoney <nuthsaid@outlook.com>
2020-08-07 23:13:59 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var indexController = require("../controller/index_controller");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/', indexController.displayIndex);
+
+
 
 module.exports = router;
